7a61fbc7417e55b8338734d0661299e1a096e464
Georgi Filev <georgi.filev00@gmail.com>
2024-02-04 19:44:47 +0200

    deployment try 25

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
+const express = require('express');
+const cors = require('cors');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer('./db.json');
-const middlewares = jsonServer.defaults({
-    static: './build'
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001; // Choose a port for your Express server
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Serve the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Use "/api/" route for JSON server
+app.use('/api', jsonServer.router('data/db.json'));
+
+// For any other route, serve the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 8000;
-
-server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-    })
-);
-server.use(router);
-
-server.listen(port, () => {
-    console.log('Server is running');
+// Start the Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
