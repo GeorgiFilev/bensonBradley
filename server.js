6f03fa9050099d7a478a1d1aca9b5e6bda97d516
Georgi Filev <georgi.filev00@gmail.com>
2024-02-04 18:39:43 +0200

    deployment try 18

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer('./data/db.json');
+const router = jsonServer('./db.json');
 const middlewares = jsonServer.defaults({
     static: './build'
 });
