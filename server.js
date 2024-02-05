33723d638597f22b0ba0c1dca04bd458d5ae4556
Georgi Filev <georgi.filev00@gmail.com>
2024-02-05 16:47:45 +0200

    deployment try 50

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router("./data/db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+    static: "./build",
+});
 const cors = require("cors");
 
 // server.use(
@@ -14,8 +16,15 @@ const cors = require("cors");
 //     }),
 // );
 // server.options("*", cors());
-server.use(cors());
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/blogs/**": "/$1",
+    })
+)
+
+server.use(cors());
+
 server.use(router);
 server.listen(process.env.REACT_APP_API_PORT, () => {
     console.log(`JSON Server is running on ${process.env.REACT_APP_API_PORT}`);
